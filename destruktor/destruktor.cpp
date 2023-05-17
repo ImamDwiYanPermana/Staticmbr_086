73908f293e52e0c3a8445d0dc79405441f451281
ImamDwiYanPermana <imampermana224@gmail.com>
2023-05-17 17:40:11 +0700

    Menambahkan 4 static di class angka

---
+++ destruktor/destruktor.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+class angka {
+private:
+    int *arr;
+    int panjang;
+public :
+    angka(int);// construktor 
+    ~angka(); // Destruktor 
+    void cetakData();
+    void isiData();
+};
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
+
